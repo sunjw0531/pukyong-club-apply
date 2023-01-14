9f39d99aecc3e6a259a3f9d8e11e964ee3af4c93
정승우 <sunjw0531@gmail.com>
2023-01-14 19:58:21 +0900

    Initial setting
    1. Install somethings
    2. Delete some files
    3. Add some commands on package.json
    4. Create Home page but nothing there

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+function Home() {
+  return (
+    <>
+      <div>asdfasdf</div>
+    </>
+  );
+}
+
+export default Home;
