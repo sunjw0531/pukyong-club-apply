be907373f19e7e0713221b102154996f87bd8402
정승우 <sunjw0531@gmail.com>
2023-01-28 22:24:50 +0900

    Design : Change CSS with 'react bootstrap' and 'styled-components'
    Change CSS with 'react bootstrap' and 'styled-components'

--- src/components/ApplyForm.js
+++ src/components/ApplyForm.js
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import emailjs from '@emailjs/browser';
 
 function ApplyForm() {
@@ -39,54 +41,55 @@ function ApplyForm() {
         </FormDiv>
       ) : (
         <FormDiv>
-          <h1 style={{ margin: 0 }}>동아리 지원서</h1>
-          <p>동아리에 지원해주셔서 감사합니다.</p>
-          <form
+          <Form
             onSubmit={handleSubmit((data) => {
               sendApplication(data);
               setIsSent(true);
             })}
           >
-            <div style={{ backgroundColor: '#F5F5DC' }}>
+            <TitleDiv>
+              <h1 style={{ margin: 0 }}>동아리 지원서</h1>
+            </TitleDiv>
+            <FormDivMain>
               <WritingDiv>
-                <label htmlFor="name">이름</label>
+                <LabelText htmlFor="name">이름</LabelText>
                 <FormInputS
                   id="name"
                   {...register('name', { required: true })}
-                  placeholder="이름"
+                  placeholder="이름을 입력해주세요."
                 />
                 {errors.name?.type === 'required' && (
                   <ErrorMessage role="alert">이름을 입력해주세요.</ErrorMessage>
                 )}
               </WritingDiv>
               <WritingDiv>
-                <label htmlFor="age">나이</label>
+                <LabelText htmlFor="age">나이</LabelText>
                 <FormInputS
                   id="age"
                   {...register('age', { required: true })}
-                  placeholder="나이"
+                  placeholder="나이를 입력해주세요. *남성일 경우 군필 여부도!"
                 />
                 {errors.age?.type === 'required' && (
                   <ErrorMessage role="alert">나이를 입력해주세요.</ErrorMessage>
                 )}
               </WritingDiv>
               <WritingDiv>
-                <label htmlFor="major">학과</label>
+                <LabelText htmlFor="major">학과</LabelText>
                 <FormInputS
                   id="major"
                   {...register('major', { required: true })}
-                  placeholder="학과"
+                  placeholder="학과 혹은 전공을 입력해주세요."
                 />
                 {errors.major?.type === 'required' && (
                   <ErrorMessage role="alert">학과를 입력해주세요.</ErrorMessage>
                 )}
               </WritingDiv>
               <WritingDiv>
-                <label htmlFor="major">학번</label>
+                <LabelText htmlFor="major">학번</LabelText>
                 <FormInputS
                   id="studentNumber"
                   {...register('studentNumber', { required: true })}
-                  placeholder="학번"
+                  placeholder="학번을 입력해주세요."
                 />
                 {errors.studentNumber?.type === 'required' && (
                   <ErrorMessage role="alert">학번을 입력해주세요.</ErrorMessage>
@@ -94,37 +97,38 @@ function ApplyForm() {
                 {}
               </WritingDiv>
               <WritingDiv>
-                <label htmlFor="hobby">취미 및 하는 게임</label>
+                <LabelText htmlFor="hobby">취미 및 하는 게임</LabelText>
                 <FormInputS
                   id="hobby"
                   {...register('hobby', { required: true })}
-                  placeholder="취미 및 하는 게임"
+                  placeholder="취미 및 하는 게임을 입력해주세요."
                 />
                 {errors.hobby?.type === 'required' && (
                   <ErrorMessage role="alert">취미를 입력해주세요.</ErrorMessage>
                 )}
               </WritingDiv>
               <WritingDiv>
-                <label htmlFor="where">주로 게임하는 장소</label>
+                <LabelText htmlFor="where">주로 게임하는 장소</LabelText>
                 <br />
-                <input
+                <Form.Check
                   type="checkbox"
                   {...register('where', { required: true })}
                   value="집"
+                  label="집"
                 />
-                집
-                <input
+                <Form.Check
                   type="checkbox"
                   {...register('where', { required: true })}
                   value="피시방"
+                  label="피시방"
                 />
-                피시방
+
                 {errors.where?.type === 'required' && (
                   <ErrorMessage role="alert">장소를 선택해주세요.</ErrorMessage>
                 )}
               </WritingDiv>
               <WritingDiv>
-                <label htmlFor="call">연락처</label>
+                <LabelText htmlFor="call">연락처</LabelText>
                 <FormInputS
                   id="call"
                   {...register('call', { required: true })}
@@ -138,15 +142,19 @@ function ApplyForm() {
               </WritingDiv>
 
               <WritingDiv>
-                <label htmlFor="introduce">자기소개 및 하고 싶은 말</label>
+                <LabelText htmlFor="introduce">
+                  자기소개 및 하고 싶은 말
+                </LabelText>
                 <DivTextarea>
                   <FormTextarea
+                    as="textarea"
                     id="introduce"
                     maxLength="400"
                     rows="10"
                     cols="50"
                     {...register('introduce', { required: true })}
-                    placeholder="400자 이내로 입력해주세요."
+                    placeholder=" 400자 이내로 입력해주세요.
+                    짧아도 괜찮습니다."
                     onChange={(e) => setTALength(e.target.value.length)}
                   />
                   <span>{tALength === '' ? 0 : tALength} / 400자</span>
@@ -156,10 +164,9 @@ function ApplyForm() {
                   )}
                 </DivTextarea>
               </WritingDiv>
-            </div>
-
-            <button type="submit">지원하기</button>
-          </form>
+            </FormDivMain>
+            <SubmitBtn type="submit">지원하기</SubmitBtn>
+          </Form>
         </FormDiv>
       )}
     </>
@@ -170,27 +177,47 @@ export default ApplyForm;
 
 const FormDiv = styled.div`
   background-color: #bac6f1;
-  height: 100vh;
+  height: 100%;
   padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const TitleDiv = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const FormDivMain = styled.div`
+  background-color: white;
+  display: grid;
+  justify-content: center;
+  width: 800px;
+  padding-top: 30px;
+`;
+
+const LabelText = styled(Form.Label)`
+  font-size: 20px;
 `;
 
 const WritingDiv = styled.div`
   margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
-const FormInputS = styled.input`
+const FormInputS = styled(Form.Control)`
   border: none;
-  border-bottom: 1px solid black;
-  padding: 5px;
-  margin-left: 5px;
-  width: 250px;
+  border: 1px solid palevioletred;
 `;
 
-const FormTextarea = styled.textarea`
+const FormTextarea = styled(Form.Control)`
   border: none;
-  border-bottom: 1px solid black;
+  border: 1px solid palevioletred;
   padding: 5px;
   margin-right: 7px;
+  border-radius: 7px;
+  width: 400px;
 `;
 
 const DivTextarea = styled.div`
@@ -200,4 +227,16 @@ const DivTextarea = styled.div`
 
 const ErrorMessage = styled.p`
   color: red;
+`;
+
+const SubmitBtn = styled(Button)`
+  color: palevioletred;
+  font-size: 1em;
+  margin-top: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  background-color: white;
+  width: 800px;
+  height: 50px;
 `;
